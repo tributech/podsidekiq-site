9d1dfbd5d6c9f3fdb80b21c6d98cbf3da86ead15
Colin Goudie <colin.goudie@tributech.com.au>
2024-02-26 12:57:29 +1100

    Some fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fabform.io",
   server: { host: '0.0.0.0'},
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), icon({
+    include: {
+      mdi: ["*"],
+      bi: ["*"],
+      ci: ["*"],
+      'simple-icons': ["*"]
+    },
+  })],
 });
