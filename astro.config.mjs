e09e92938ac0cf5ba6ef9caa3ce7464f48bed31b
Colin Goudie <colin.goudie@tributech.com.au>
2024-03-11 15:48:14 +1100

    Pulled out Google for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import Google from '@auth/core/providers/google';
+import Auth from "@auth/core"
+import Google from "@auth/core/providers/google"
 
 
 // https://astro.build/config
@@ -20,8 +21,8 @@ export default defineConfig({
       teenyicons: ["*"]
     },
   })],
-  providers: [
-		GoogleProvider({
+  providers: { 
+		google: Google({
 			clientId: import.meta.env.GOOGLE_CLIENT_ID,
 			clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
       authorization: {
@@ -32,5 +33,5 @@ export default defineConfig({
         }
       }
 		}),
-	],
+  },
 });
